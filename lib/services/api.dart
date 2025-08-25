f32646e67885f48ffbf31f9823ece3803c26a512
mohamed shehata <moshehata7272d@gmail.com>
2025-08-24 20:36:18 -0700

    put request in api class

--- lib/services/api.dart
+++ lib/services/api.dart
@@ -38,4 +38,30 @@ class Api {
       );
     }
   }
+
+  Future<dynamic> put({
+    required String url,
+    @required dynamic body,
+    @required String? token,
+  }) async {
+    final Map<String, dynamic> headers = {};
+    headers.addAll({
+      " Content-Type": " application/x-www-form-urlencoded"
+      });
+    if (token != null) {
+      headers.addAll({"authorization": "Bearer  $token"});
+    }
+    Response response = await dio.put(
+      url,
+      data: body,
+      options: Options(headers: headers),
+    );
+    if (response.statusCode == 200) {
+      return response.data;
+    } else {
+      throw Exception(
+        "there is an error with status code ${response.statusCode}",
+      );
+    }
+  }
 }
